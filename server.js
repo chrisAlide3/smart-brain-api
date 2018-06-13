5afaf4f1ee5ad9141ce18b20a404bbcead4826e6
chris <christian.bracher@gmail.com>
2018-06-13 08:20:25 +0800

    setting PORT and root res.send

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Defining routes
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {res.send('it is working!')})
 
 //Register (we pass db and bcrypt. called dependency injection)
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
